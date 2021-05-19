04a37706818becf3d12595705d6711f40cb023bb
nina.cressoni <nina.cressoni@writesys.com.br>
2021-05-19 14:21:48 -0300

    [hidden] binding

--- src/app/events/events-thumbnail.components.ts
+++ src/app/events/events-thumbnail.components.ts
@@ -9,12 +9,12 @@ import { Component, Input } from "@angular/core";
         <div>Date: {{event?.date}} </div>
         <div>Time: {{event?.time}} </div>
         <div>Price: {{event?.price}}  </div>
-        <div *ngIf="event?.location">
+        <div [hidden]="!event?.location" >
             <span>Location: {{event?.location?.address}} </span>
             <span class="pad-left"> {{event?.location?.city}}, {{event?.location?.country}} </span>
         </div>
 
-        <div *ngIf="event?.onlineUrl">
+        <div [hidden]="!event.onlineUrl">
             Online URL: {{event?.onlineUrl}}
         </div>
     </div>
