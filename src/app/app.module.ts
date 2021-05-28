91f165eef266bb0bc5ce5c25dfb985e1cb48993b
nina.cressoni <nina.cressoni@writesys.com.br>
2021-05-28 15:09:46 -0300

    adding Routing and navigating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.components';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,11 +17,13 @@ import { EventService } from './events/shared/event.service';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
