acdf257a505559089ee0e3a998c842e7feb7f453
nina.cressoni <nina.cressoni@writesys.com.br>
2021-06-07 15:38:46 -0300

    Lazily loading feature modules

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -8,11 +8,16 @@ import { EventsListComponent } from "./events/events-list.component";
 
 export const appRoutes:Routes = [
     { path: 'events/new', component: CreateEventComponent, 
-    canDeactivate: ['canDeactivateCreateEvent'] },
+        canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent, resolve:
-    {events:EventListResolver} },
+        {events:EventListResolver} },
     { path: 'events/:id', component: EventDetailsComponent, 
-    canActivate: [EventRouteActivator] },
+        canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { 
+        path: 'user', 
+        loadChildren: () => import('./user/user.module')
+        .then(m => m.UserModule)
+    }
 ]
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ProfileComponent } from "./profile.component";
+import { userRoutes } from "./user.routes";
+
+@NgModule ({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(userRoutes)
+    ],
+    declarations: [
+        ProfileComponent
+    ],
+    providers: [
+
+    ]
+
+})
+
+export class UserModule {}
